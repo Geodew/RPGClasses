Geodew/RPGClasses

commit a14988265941fadfd933059bfd117832a541fccc

Tentative multiplayer visual effects fix - Clients will now trade class/level information with loaded information as they would with fresh characters ---> TODO: Check for duplicated damage effects or projectiles or dust

PacketMessages/PlayerClassLevelInfoNetMsg.cs
@@ -1,0 +1,312 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RPG.PacketMessages
+{
+    class PlayerClassLevelInfoNetMsg
+    {
+        private const PacketMessageTypeEnum mPacketMessageType = PacketMessageTypeEnum.PLAYER_CLASS_LEVEL_INFO;
+
+        private int mPlayerId;
+        private bool mRequestPeerInfo;
+        //zzz add version information so we know what classes to expect from the other person and in what order
+
+        // Omitting the standard `m` in front of these symbols for easy copy-paste from MPlayer.cs...
+        //zzz Consider moving these bools to a struct for common use instead.
+        #region Class flags
+        private bool knight;
+        private bool berserker;
+        private bool fortress;
+        private bool sage;
+        private bool warmage;
+        private bool conjuror;
+        private bool spiritMage;
+        private bool contractedSword;
+        private bool wanderer;
+        private bool marksman;
+        private bool ranger;
+        private bool arcaneSniper;
+        private bool savage;
+        private bool ninja;
+        private bool rogue;
+        private bool soulbound;
+        private bool explorer;
+        private bool cavalry;
+        private bool merman;
+        private bool werewolf;
+        private bool harpy;
+        private bool angel;
+        private bool demon;
+        private bool dwarf;
+        private bool bloodKnight;
+        private bool taintedElf;
+        private bool hallowMage;
+        private bool pharaoh;
+        private bool pirate;
+        private bool jungleShaman;
+        private bool viking;
+        private bool truffle;
+        private bool dragoon;
+        private bool chronomancer;
+        private bool angler;
+        private bool celestial;
+        private bool voidwalker;
+        private bool moth;
+        private bool monk;
+        private bool warpKnight;
+        private bool heritor;
+        #endregion
+        #region Killed boss flags
+        private bool killedSlime;
+        private bool killedEye;
+        private bool killedWormOrBrain;
+        private bool killedSkelly;
+        private bool killedBee;
+        private bool killedWall;
+        private bool killedDestroyer;
+        private bool killedTwins;
+        private bool killedPrime;
+        private bool killedPlant;
+        private bool killedGolem;
+        private bool killedFish;
+        private bool killedCultist;
+        private bool killedMoon;
+        #endregion
+
+
+        private void Process(
+                int whoAmI,
+                Mod mod)
+        {
+            Player player = Main.player[mPlayerId];
+            MPlayer modPlayer = (MPlayer)player.GetModPlayer(mod, "MPlayer");
+            
+            #region Class flags
+            modPlayer.knight = knight;
+            modPlayer.berserker = berserker;
+            modPlayer.fortress = fortress;
+            modPlayer.sage = sage;
+            modPlayer.warmage = warmage;
+            modPlayer.conjuror = conjuror;
+            modPlayer.spiritMage = spiritMage;
+            modPlayer.contractedSword = contractedSword;
+            modPlayer.wanderer = wanderer;
+            modPlayer.marksman = marksman;
+            modPlayer.ranger = ranger;
+            modPlayer.arcaneSniper = arcaneSniper;
+            modPlayer.savage = savage;
+            modPlayer.ninja = ninja;
+            modPlayer.rogue = rogue;
+            modPlayer.soulbound = soulbound;
+            modPlayer.explorer = explorer;
+            modPlayer.cavalry = cavalry;
+            modPlayer.merman = merman;
+            modPlayer.werewolf = werewolf;
+            modPlayer.harpy = harpy;
+            modPlayer.angel = angel;
+            modPlayer.demon = demon;
+            modPlayer.dwarf = dwarf;
+            modPlayer.bloodKnight = bloodKnight;
+            modPlayer.taintedElf = taintedElf;
+            modPlayer.hallowMage = hallowMage;
+            modPlayer.pharaoh = pharaoh;
+            modPlayer.pirate = pirate;
+            modPlayer.jungleShaman = jungleShaman;
+            modPlayer.viking = viking;
+            modPlayer.truffle = truffle;
+            modPlayer.dragoon = dragoon;
+            modPlayer.chronomancer = chronomancer;
+            modPlayer.angler = angler;
+            modPlayer.celestial = celestial;
+            modPlayer.voidwalker = voidwalker;
+            modPlayer.moth = moth;
+            modPlayer.monk = monk;
+            modPlayer.warpKnight = warpKnight;
+            modPlayer.heritor = heritor;
+            #endregion
+            #region Killed boss flags
+            modPlayer.killedSlime = killedSlime;
+            modPlayer.killedEye = killedEye;
+            modPlayer.killedWormOrBrain = killedWormOrBrain;
+            modPlayer.killedSkelly = killedSkelly;
+            modPlayer.killedBee = killedBee;
+            modPlayer.killedWall = killedWall;
+            modPlayer.killedDestroyer = killedDestroyer;
+            modPlayer.killedTwins = killedTwins;
+            modPlayer.killedPrime = killedPrime;
+            modPlayer.killedPlant = killedPlant;
+            modPlayer.killedGolem = killedGolem;
+            modPlayer.killedFish = killedFish;
+            modPlayer.killedCultist = killedCultist;
+            modPlayer.killedMoon = killedMoon;
+            #endregion
+
+            // If sender requested peer info, rebroadcast ours
+            if (mRequestPeerInfo)
+            {
+                Player localPlayer = Main.player[Main.myPlayer];
+                MPlayer localModPlayer = (MPlayer)localPlayer.GetModPlayer(mod, "MPlayer");
+
+                SerializeAndSend(
+                    mod,
+                    localModPlayer,
+                    false);
+            }
+        }
+
+        public void HandlePacket(
+                BinaryReader reader,
+                int whoAmI,
+                Mod mod)
+        {
+            Deserialize(
+                reader,
+                whoAmI);
+            Process(
+                whoAmI,
+                mod);
+        }
+
+        public static void SerializeAndSend(
+                Mod mod,
+                MPlayer modPlayer,
+                bool requestPeerInfo)
+        {
+            ModPacket newPacket = mod.GetPacket();
+
+            newPacket.Write(modPlayer.player.whoAmI);
+            newPacket.Write(requestPeerInfo);
+
+            #region Class flags
+            newPacket.Write(modPlayer.knight);
+            newPacket.Write(modPlayer.berserker);
+            newPacket.Write(modPlayer.fortress);
+            newPacket.Write(modPlayer.sage);
+            newPacket.Write(modPlayer.warmage);
+            newPacket.Write(modPlayer.conjuror);
+            newPacket.Write(modPlayer.spiritMage);
+            newPacket.Write(modPlayer.contractedSword);
+            newPacket.Write(modPlayer.wanderer);
+            newPacket.Write(modPlayer.marksman);
+            newPacket.Write(modPlayer.ranger);
+            newPacket.Write(modPlayer.arcaneSniper);
+            newPacket.Write(modPlayer.savage);
+            newPacket.Write(modPlayer.ninja);
+            newPacket.Write(modPlayer.rogue);
+            newPacket.Write(modPlayer.soulbound);
+            newPacket.Write(modPlayer.explorer);
+            newPacket.Write(modPlayer.cavalry);
+            newPacket.Write(modPlayer.merman);
+            newPacket.Write(modPlayer.werewolf);
+            newPacket.Write(modPlayer.harpy);
+            newPacket.Write(modPlayer.angel);
+            newPacket.Write(modPlayer.demon);
+            newPacket.Write(modPlayer.dwarf);
+            newPacket.Write(modPlayer.bloodKnight);
+            newPacket.Write(modPlayer.taintedElf);
+            newPacket.Write(modPlayer.hallowMage);
+            newPacket.Write(modPlayer.pharaoh);
+            newPacket.Write(modPlayer.pirate);
+            newPacket.Write(modPlayer.jungleShaman);
+            newPacket.Write(modPlayer.viking);
+            newPacket.Write(modPlayer.truffle);
+            newPacket.Write(modPlayer.dragoon);
+            newPacket.Write(modPlayer.chronomancer);
+            newPacket.Write(modPlayer.angler);
+            newPacket.Write(modPlayer.celestial);
+            newPacket.Write(modPlayer.voidwalker);
+            newPacket.Write(modPlayer.moth);
+            newPacket.Write(modPlayer.monk);
+            newPacket.Write(modPlayer.warpKnight);
+            newPacket.Write(modPlayer.heritor);
+            #endregion
+            #region Killed boss flags
+            newPacket.Write(modPlayer.killedSlime);
+            newPacket.Write(modPlayer.killedEye);
+            newPacket.Write(modPlayer.killedWormOrBrain);
+            newPacket.Write(modPlayer.killedSkelly);
+            newPacket.Write(modPlayer.killedBee);
+            newPacket.Write(modPlayer.killedWall);
+            newPacket.Write(modPlayer.killedDestroyer);
+            newPacket.Write(modPlayer.killedTwins);
+            newPacket.Write(modPlayer.killedPrime);
+            newPacket.Write(modPlayer.killedPlant);
+            newPacket.Write(modPlayer.killedGolem);
+            newPacket.Write(modPlayer.killedFish);
+            newPacket.Write(modPlayer.killedCultist);
+            newPacket.Write(modPlayer.killedMoon);
+            #endregion
+
+            newPacket.Send();
+        }
+
+        private void Deserialize(
+                BinaryReader reader,
+                int whoAmI)
+        {
+            mPlayerId = reader.ReadInt32();
+            mRequestPeerInfo = reader.ReadBoolean();
+
+            #region Class flags
+            knight = reader.ReadBoolean();
+            berserker = reader.ReadBoolean();
+            fortress = reader.ReadBoolean();
+            sage = reader.ReadBoolean();
+            warmage = reader.ReadBoolean();
+            conjuror = reader.ReadBoolean();
+            spiritMage = reader.ReadBoolean();
+            contractedSword = reader.ReadBoolean();
+            wanderer = reader.ReadBoolean();
+            marksman = reader.ReadBoolean();
+            ranger = reader.ReadBoolean();
+            arcaneSniper = reader.ReadBoolean();
+            savage = reader.ReadBoolean();
+            ninja = reader.ReadBoolean();
+            rogue = reader.ReadBoolean();
+            soulbound = reader.ReadBoolean();
+            explorer = reader.ReadBoolean();
+            cavalry = reader.ReadBoolean();
+            merman = reader.ReadBoolean();
+            werewolf = reader.ReadBoolean();
+            harpy = reader.ReadBoolean();
+            angel = reader.ReadBoolean();
+            demon = reader.ReadBoolean();
+            dwarf = reader.ReadBoolean();
+            bloodKnight = reader.ReadBoolean();
+            taintedElf = reader.ReadBoolean();
+            hallowMage = reader.ReadBoolean();
+            pharaoh = reader.ReadBoolean();
+            pirate = reader.ReadBoolean();
+            jungleShaman = reader.ReadBoolean();
+            viking = reader.ReadBoolean();
+            truffle = reader.ReadBoolean();
+            dragoon = reader.ReadBoolean();
+            chronomancer = reader.ReadBoolean();
+            angler = reader.ReadBoolean();
+            celestial = reader.ReadBoolean();
+            voidwalker = reader.ReadBoolean();
+            moth = reader.ReadBoolean();
+            monk = reader.ReadBoolean();
+            warpKnight = reader.ReadBoolean();
+            heritor = reader.ReadBoolean();
+            #endregion
+            #region Killed boss flags
+            killedSlime = reader.ReadBoolean();
+            killedEye = reader.ReadBoolean();
+            killedWormOrBrain = reader.ReadBoolean();
+            killedSkelly = reader.ReadBoolean();
+            killedBee = reader.ReadBoolean();
+            killedWall = reader.ReadBoolean();
+            killedDestroyer = reader.ReadBoolean();
+            killedTwins = reader.ReadBoolean();
+            killedPrime = reader.ReadBoolean();
+            killedPlant = reader.ReadBoolean();
+            killedGolem = reader.ReadBoolean();
+            killedFish = reader.ReadBoolean();
+            killedCultist = reader.ReadBoolean();
+            killedMoon = reader.ReadBoolean();
+            #endregion
+        }
+    }
+}

PacketMessages/VelocityChangeNpcNetMsg.cs
@@ -60,6 +60,8 @@ namespace RPG.PacketMessages
             newPacket.Write(newVelocityX);
             newPacket.Write(newVelocityY);
             newPacket.Write(npcIndex);
+
+            newPacket.Send();
         }
 
         private void Deserialize(

commit 34afb56670d55762d8f7c05eb6ac15361ca7c71d

Tentative multiplayer visual effects fix 2 - Fix multiplayer crash introduced in the previous revision a149882 ---> (Don't send packets in single-player, it can crash in load menus)

PacketMessages/HealPlayerNetMsg.cs
@@ -38,12 +38,15 @@ namespace RPG.PacketMessages
                 int playerId,
                 int healAmount)
         {
-            ModPacket newPacket = mod.GetPacket();
+            if (Main.netMode != 0)  // Not single-player
+            {
+                ModPacket newPacket = mod.GetPacket();
 
-            newPacket.Write(playerId);
-            newPacket.Write(healAmount);
+                newPacket.Write(playerId);
+                newPacket.Write(healAmount);
 
-            newPacket.Send();
+                newPacket.Send();
+            }
         }
 
         private void Deserialize(

PacketMessages/LevelUpPlayerNetMsg.cs
@@ -151,12 +151,15 @@ namespace RPG.PacketMessages
                 int playerId,
                 BossGroupEnum bossDefeated)
         {
-            ModPacket newPacket = mod.GetPacket();
+            if (Main.netMode != 0)  // Not single-player
+            {
+                ModPacket newPacket = mod.GetPacket();
 
-            newPacket.Write(playerId);
-            newPacket.Write((int)bossDefeated);
+                newPacket.Write(playerId);
+                newPacket.Write((int)bossDefeated);
 
-            newPacket.Send();
+                newPacket.Send();
+            }
         }
 
         private void Deserialize(

PacketMessages/PlayerClassLevelInfoNetMsg.cs
@@ -173,72 +173,75 @@ namespace RPG.PacketMessages
                 MPlayer modPlayer,
                 bool requestPeerInfo)
         {
-            ModPacket newPacket = mod.GetPacket();
+            if (Main.netMode != 0)  // Not single-player
+            {
+                ModPacket newPacket = mod.GetPacket();
 
-            newPacket.Write(modPlayer.player.whoAmI);
-            newPacket.Write(requestPeerInfo);
+                newPacket.Write(modPlayer.player.whoAmI);
+                newPacket.Write(requestPeerInfo);
 
-            #region Class flags
-            newPacket.Write(modPlayer.knight);
-            newPacket.Write(modPlayer.berserker);
-            newPacket.Write(modPlayer.fortress);
-            newPacket.Write(modPlayer.sage);
-            newPacket.Write(modPlayer.warmage);
-            newPacket.Write(modPlayer.conjuror);
-            newPacket.Write(modPlayer.spiritMage);
-            newPacket.Write(modPlayer.contractedSword);
-            newPacket.Write(modPlayer.wanderer);
-            newPacket.Write(modPlayer.marksman);
-            newPacket.Write(modPlayer.ranger);
-            newPacket.Write(modPlayer.arcaneSniper);
-            newPacket.Write(modPlayer.savage);
-            newPacket.Write(modPlayer.ninja);
-            newPacket.Write(modPlayer.rogue);
-            newPacket.Write(modPlayer.soulbound);
-            newPacket.Write(modPlayer.explorer);
-            newPacket.Write(modPlayer.cavalry);
-            newPacket.Write(modPlayer.merman);
-            newPacket.Write(modPlayer.werewolf);
-            newPacket.Write(modPlayer.harpy);
-            newPacket.Write(modPlayer.angel);
-            newPacket.Write(modPlayer.demon);
-            newPacket.Write(modPlayer.dwarf);
-            newPacket.Write(modPlayer.bloodKnight);
-            newPacket.Write(modPlayer.taintedElf);
-            newPacket.Write(modPlayer.hallowMage);
-            newPacket.Write(modPlayer.pharaoh);
-            newPacket.Write(modPlayer.pirate);
-            newPacket.Write(modPlayer.jungleShaman);
-            newPacket.Write(modPlayer.viking);
-            newPacket.Write(modPlayer.truffle);
-            newPacket.Write(modPlayer.dragoon);
-            newPacket.Write(modPlayer.chronomancer);
-            newPacket.Write(modPlayer.angler);
-            newPacket.Write(modPlayer.celestial);
-            newPacket.Write(modPlayer.voidwalker);
-            newPacket.Write(modPlayer.moth);
-            newPacket.Write(modPlayer.monk);
-            newPacket.Write(modPlayer.warpKnight);
-            newPacket.Write(modPlayer.heritor);
-            #endregion
-            #region Killed boss flags
-            newPacket.Write(modPlayer.killedSlime);
-            newPacket.Write(modPlayer.killedEye);
-            newPacket.Write(modPlayer.killedWormOrBrain);
-            newPacket.Write(modPlayer.killedSkelly);
-            newPacket.Write(modPlayer.killedBee);
-            newPacket.Write(modPlayer.killedWall);
-            newPacket.Write(modPlayer.killedDestroyer);
-            newPacket.Write(modPlayer.killedTwins);
-            newPacket.Write(modPlayer.killedPrime);
-            newPacket.Write(modPlayer.killedPlant);
-            newPacket.Write(modPlayer.killedGolem);
-            newPacket.Write(modPlayer.killedFish);
-            newPacket.Write(modPlayer.killedCultist);
-            newPacket.Write(modPlayer.killedMoon);
-            #endregion
+                #region Class flags
+                newPacket.Write(modPlayer.knight);
+                newPacket.Write(modPlayer.berserker);
+                newPacket.Write(modPlayer.fortress);
+                newPacket.Write(modPlayer.sage);
+                newPacket.Write(modPlayer.warmage);
+                newPacket.Write(modPlayer.conjuror);
+                newPacket.Write(modPlayer.spiritMage);
+                newPacket.Write(modPlayer.contractedSword);
+                newPacket.Write(modPlayer.wanderer);
+                newPacket.Write(modPlayer.marksman);
+                newPacket.Write(modPlayer.ranger);
+                newPacket.Write(modPlayer.arcaneSniper);
+                newPacket.Write(modPlayer.savage);
+                newPacket.Write(modPlayer.ninja);
+                newPacket.Write(modPlayer.rogue);
+                newPacket.Write(modPlayer.soulbound);
+                newPacket.Write(modPlayer.explorer);
+                newPacket.Write(modPlayer.cavalry);
+                newPacket.Write(modPlayer.merman);
+                newPacket.Write(modPlayer.werewolf);
+                newPacket.Write(modPlayer.harpy);
+                newPacket.Write(modPlayer.angel);
+                newPacket.Write(modPlayer.demon);
+                newPacket.Write(modPlayer.dwarf);
+                newPacket.Write(modPlayer.bloodKnight);
+                newPacket.Write(modPlayer.taintedElf);
+                newPacket.Write(modPlayer.hallowMage);
+                newPacket.Write(modPlayer.pharaoh);
+                newPacket.Write(modPlayer.pirate);
+                newPacket.Write(modPlayer.jungleShaman);
+                newPacket.Write(modPlayer.viking);
+                newPacket.Write(modPlayer.truffle);
+                newPacket.Write(modPlayer.dragoon);
+                newPacket.Write(modPlayer.chronomancer);
+                newPacket.Write(modPlayer.angler);
+                newPacket.Write(modPlayer.celestial);
+                newPacket.Write(modPlayer.voidwalker);
+                newPacket.Write(modPlayer.moth);
+                newPacket.Write(modPlayer.monk);
+                newPacket.Write(modPlayer.warpKnight);
+                newPacket.Write(modPlayer.heritor);
+                #endregion
+                #region Killed boss flags
+                newPacket.Write(modPlayer.killedSlime);
+                newPacket.Write(modPlayer.killedEye);
+                newPacket.Write(modPlayer.killedWormOrBrain);
+                newPacket.Write(modPlayer.killedSkelly);
+                newPacket.Write(modPlayer.killedBee);
+                newPacket.Write(modPlayer.killedWall);
+                newPacket.Write(modPlayer.killedDestroyer);
+                newPacket.Write(modPlayer.killedTwins);
+                newPacket.Write(modPlayer.killedPrime);
+                newPacket.Write(modPlayer.killedPlant);
+                newPacket.Write(modPlayer.killedGolem);
+                newPacket.Write(modPlayer.killedFish);
+                newPacket.Write(modPlayer.killedCultist);
+                newPacket.Write(modPlayer.killedMoon);
+                #endregion
 
-            newPacket.Send();
+                newPacket.Send();
+            }
         }
 
         private void Deserialize(

PacketMessages/SandstormVisualsNetMsg.cs
@@ -45,11 +45,14 @@ namespace RPG.PacketMessages
                 Mod mod,
                 int playerId)
         {
-            ModPacket newPacket = mod.GetPacket();
+            if (Main.netMode != 0)  // Not single-player
+            {
+                ModPacket newPacket = mod.GetPacket();
 
-            newPacket.Write(playerId);
+                newPacket.Write(playerId);
 
-            newPacket.Send();
+                newPacket.Send();
+            }
         }
 
         private void Deserialize(

PacketMessages/VelocityChangeNpcNetMsg.cs
@@ -54,14 +54,17 @@ namespace RPG.PacketMessages
                 float newVelocityY,
                 int npcIndex)
         {
-            ModPacket newPacket = mod.GetPacket();
+            if (Main.netMode != 0)  // Not single-player
+            {
+                ModPacket newPacket = mod.GetPacket();
 
-            newPacket.Write((int)mPacketMessageType);
-            newPacket.Write(newVelocityX);
-            newPacket.Write(newVelocityY);
-            newPacket.Write(npcIndex);
+                newPacket.Write((int)mPacketMessageType);
+                newPacket.Write(newVelocityX);
+                newPacket.Write(newVelocityY);
+                newPacket.Write(npcIndex);
 
-            newPacket.Send();
+                newPacket.Send();
+            }
         }
 
         private void Deserialize(