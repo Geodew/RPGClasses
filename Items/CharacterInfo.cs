3d4311b7a2f17a03df7b502e026bf9d236a0fdfc
Geodew <geodew@gmail.com>
2018-11-25 23:16:00 -0500

    Character Info improvements... - Minimum level displayed as 1 instead of 0 - Display melee speed as a percentage gain instead of a fractional decrease to time - Display move speed as a percentage gain - Fixed life regeneration formula (used to be "not perfectly precise")

--- Items/CharacterInfo.cs
+++ Items/CharacterInfo.cs
@@ -22,7 +22,7 @@ namespace RPG.Items
         public override bool UseItem(Player p)
         {
             MPlayer mplayer = (MPlayer)(p.GetModPlayer(mod, "MPlayer"));
-            Main.NewText("Level " + mplayer.specialProgressionCount);//some classes will exceed 14
+            Main.NewText("Level " + (mplayer.specialProgressionCount + 1));  // Some classes will exceed 15
             string killedBosses = "";
             if (mplayer.killedEye) { killedBosses += "Eye of Cthulu, "; }
             if (mplayer.killedSlime) { killedBosses += "Slime King, "; }
@@ -38,7 +38,44 @@ namespace RPG.Items
             if (mplayer.killedFish) { killedBosses += "Duke Fishron, "; }
             if (mplayer.killedCultist) { killedBosses += "Lunatic Cultist, "; }
             if (mplayer.killedMoon) { killedBosses += "Moon Lord"; }
-            Main.NewText("Slain Bosses: "+killedBosses);
+
+            // Natural regen calculation
+            // See: https://terraria.gamepedia.com/Health_regeneration
+            float movementScalar;
+            float expertScalar;
+            int naturalRegen;
+            int effectiveRegenTime = p.lifeRegenTime / 300;  // Intentional truncation here
+            if (p.lifeRegenTime >= 2100)
+            {
+                effectiveRegenTime--;
+                if (p.lifeRegenTime >= 2700)
+                {
+                    effectiveRegenTime--;
+                    if (p.lifeRegenTime >= 3300)
+                    {
+                        effectiveRegenTime--;
+                    }
+                }
+            }
+            if (p.velocity.Length() > 1.0e-6f)
+            {
+                movementScalar = 0.5f;
+            }
+            else
+            {
+                movementScalar = 1.25f;
+            }
+            if (Main.expertMode && !p.HasBuff(BuffID.WellFed))
+            {
+                expertScalar = 0.5f;
+            }
+            else
+            {
+                expertScalar = 1.0f;
+            }
+            naturalRegen = (int)((((float)p.statLifeMax2 / 400.0f * 0.85f + 0.15f) * effectiveRegenTime * movementScalar * expertScalar) + 0.5f);
+
+            Main.NewText("Slain Bosses: " + killedBosses);
             Main.NewText("Melee damage bonus: " + (p.meleeDamage-1f)*100 + "%");
             Main.NewText("Melee crit bonus: " + (p.meleeCrit - 4) + "%");
             Main.NewText("Magic damage bonus: " + (p.magicDamage-1f)*100 + "%");
@@ -51,21 +88,12 @@ namespace RPG.Items
             Main.NewText("Thrown damage bonus: " + (p.thrownDamage - 1f)*100 + "%");
             Main.NewText("Thrown crit bonus: " + (p.thrownCrit - 4) + "%");
             Main.NewText("Max health bonus: " + (p.statLifeMax2 - p.statLifeMax));
-            int naturalRegen = (int)((p.lifeRegenTime/300)*1.25* (float)p.statLifeMax2 / 400f * 0.85f + 0.15f);
-            if (p.lifeRegenTime >= 3300)
-            {
-                naturalRegen--;
-                if (p.lifeRegenTime >= 3600)
-                {
-                    naturalRegen--;
-                }
-            }
-            Main.NewText("Life regen bonus: " + (p.lifeRegen-naturalRegen)/2.0f + " per second");//not perfectly precise
+            Main.NewText("Life regen bonus: " + ((p.lifeRegen - naturalRegen) / 2.0f) + " per second");
             Main.NewText("Max mana bonus: " + (p.statManaMax2 - p.statManaMax));
             Main.NewText("Mana regen bonus: " + (p.manaRegenBonus));
             Main.NewText("Defense bonus: " + p.statDefense);
-            Main.NewText("Melee speed bonus: " + (p.meleeSpeed - 1f));
-            Main.NewText("Move speed bonus: " + (p.moveSpeed - 1f));
+            Main.NewText("Melee speed bonus: " + (100.0f / (p.meleeSpeed - 1.0f)) + "%");
+            Main.NewText("Move speed bonus: " + ((p.moveSpeed - 1.0f) * 100.0f) + "%");
             Main.NewText("Armor penetration bonus: " + (p.armorPenetration));
             Main.NewText("Fishing skill bonus: " + (p.fishingSkill));
             Main.NewText("Flight time: " + (p.wingTimeMax / 60.0f) + " seconds");
