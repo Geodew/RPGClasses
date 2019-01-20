5172ae1ff162471d74fc8698673f3ca3d9fb0743
Geodew <geodew@gmail.com>
2019-01-20 04:16:07 -0500

    - ALL: Added multiplayer synchronization for Active Ability activation ---> Note on implementation: Dragoon and Sage had to be handled as special cases, due to being able to charge the Active Ability up - Dragoon: Fixed bug where any amount of jump charging done while airborne would be saved for the next jump if the key was released before touching the ground - Dragoon: Fixed bug where the Active Ability jump would suddenly and unintentionally double in strength after defeating the 7th boss - Sage: Fixed an incompatibility with other mods bug where mods that add max mana in amounts not divisible by 20 may cause the player to end up with additional max mana for the rest of the play session - Celestial: Fixed an incompatibility with other mods bug where the mana regen delay after using the Active Ability could change based on mod load order if other mods set the delay as part of ModPlayer processing ---> This may still happen for some mods, but that's likely the other mod's fault, then. Note the change made to the Celestial; a similar one must be made in the other mod. - Other minor runtime speed improvements

---
+++ PacketMessages/PlayerActiveAbilityStartNetMsg.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RPG.PacketMessages
+{
+    class HealPlayerNetMsg
+    {
+        private const PacketMessageTypeEnum mPacketMessageType = PacketMessageTypeEnum.HEAL_PLAYER;
+
+        private int mPlayerId;
+        private int mHealAmount;
+
+
+        private void Process(
+                int whoAmI,
+                Mod mod)
+        {
+            Player player = Main.player[mPlayerId];
+            player.statLife += mHealAmount;
+        }
+
+        public void HandlePacket(
+                BinaryReader reader,
+                int whoAmI,
+                Mod mod)
+        {
+            Deserialize(
+                reader,
+                whoAmI);
+            ServerBroadcast(
+                whoAmI,
+                mod);
+            Process(
+                whoAmI,
+                mod);
+        }
+
+        public static void SerializeAndSend(
+                Mod mod,
+                int playerId,
+                int healAmount)
+        {
+            if (Main.netMode != NetmodeID.SinglePlayer)
+            {
+                ModPacket newPacket = mod.GetPacket();
+
+                newPacket.Write((int)mPacketMessageType);
+
+                newPacket.Write(playerId);
+                newPacket.Write(healAmount);
+
+                newPacket.Send();
+            }
+        }
+
+        private void Deserialize(
+                BinaryReader reader,
+                int whoAmI)
+        {
+            mPlayerId = reader.ReadInt32();
+            mHealAmount = reader.ReadInt32();
+        }
+
+        private void ServerBroadcast(
+                int whoAmI,
+                Mod mod)
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                SerializeAndSend(
+                    mod,
+                    mPlayerId,
+                    mHealAmount);
+            }
+        }
+    }
+}
